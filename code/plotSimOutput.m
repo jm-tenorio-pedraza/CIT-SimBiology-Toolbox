b1925ec96ebf84b35d6f6ece990079648fddd7bf
Miguel <jm.tenorio.pedraza@gmail.com>
2019-08-03 14:47:00 +0200

    add plotting function

---
+++ code/plotSimOutput.m
@@ -0,0 +1,21 @@
+function plotSimOutput(data,colIndx)
+
+n_sim=size(data,1);
+n_col=ceil(sqrt(n_sim));
+n_row=ceil(n_sim/n_col);
+treatments=unique({data.Group});
+treatment_colors=linspecer(length(treatments));
+figure
+for i=1:n_sim
+    subplot(n_row,n_col,i)
+    hold on
+    sim=plot(data(i).simTime, data(i).simValue(:,colIndx));
+    dat=plot(data(i).dataTime, data(i).dataValue(:,colIndx));
+    title(data(i).Name)
+    
+    col_i=treatment_colors(ismember(treatments,data(i).Group),:);
+    sim.Color=col_i;
+    dat.LineStyle='none';
+    dat.MarkerFaceColor=col_i;
+    dat.Marker='d';
+end
