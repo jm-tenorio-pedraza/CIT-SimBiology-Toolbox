56e62c2c2555b2c8d2d17c2a886f8a8b2769b1b8
Miguel <jm.tenorio.pedraza@gmail.com>
2020-02-09 12:33:31 +0100

    m

--- output/CIM/PI/CIM3/CIM_ICB3.m
+++ output/CIM/PI/CIM3/CIM_ICB3.m
@@ -20,7 +20,7 @@ set(cs, 'MaximumWallClock', 0.25)
 %% Parameter setup
 parameters = {'kin_CD8';'KDE_MDSC';'K_CTLA4'; ...
     'kpro_Tumor'; 'kill_CD8'; 'kin_Treg' ; ...
-    'K_MDSC';'kin_MDSC'; 'f3'; 'K_PDL1'; 'kill_Treg'; 'K_CD8'};
+     'K_PDL1'; 'kill_Treg'; 'K_CD8'};
 parameters = [parameters; 'T_0'];
 
 % Define outputs% Define outputs
@@ -53,44 +53,17 @@ PI.observablesPlot={'TV' };
 
 %% Optimization setup
 % Hierarchical structure
-cell_indx = [4];
-indiv_indx = [2];
+cell_indx = [3 4];
+indiv_indx = [1];
 
 PI.H = getHierarchicalStruct(parameters(1:end-1),PI,'n_sigma', length(observables),...
     'rand_indx', indiv_indx, 'cell_indx',cell_indx, 'n_indiv', length(PI.u));
-if ~isempty(PI.H.IndividualParams(1).Index)
-        indivSigmaNames=arrayfun(@(x)strjoin({'omega', x.name}, '_'),PI.H.IndividualParams,'UniformOutput',false)';
-else
-    indivSigmaNames = [];
-end
-if ~isempty(PI.H.CellParams(1).Index)
-    cellSigmaNames=arrayfun(@(x)strjoin({'psi', x.name}, '_'),PI.H.CellParams,'UniformOutput',false)';
-else
-    cellSigmaNames = [];
-end
-try
-SigmaNames = [cellSigmaNames; indivSigmaNames];
-SigmaNames(end+1:end+length(observables),1) =  cellfun(@(x) strjoin({'sigma', x}, '_'),...
-    observables','UniformOutput', false);
-catch
-    SigmaNames=cellfun(@(x) strjoin({'sigma', x}, '_'),...
-    observables','UniformOutput', false);
-end
-% Generating PI
-alpha = [repelem(0.1, length([PI.H.CellParams(:).OmegaIndex]),1);...
-    repelem(0.1, length([PI.H.IndividualParams(:).OmegaIndex]),1);...
-    repelem(0.001, length(setdiff(PI.H.SigmaParams, [PI.H.CellParams(:).OmegaIndex ...
-    PI.H.IndividualParams(:).OmegaIndex])),1)];
-beta = [repelem(0.1, length([PI.H.CellParams(:).OmegaIndex]),1);...
-    repelem(0.1, length([PI.H.IndividualParams(:).OmegaIndex]),1);...
-    repelem(0.001, length(setdiff(PI.H.SigmaParams, [PI.H.CellParams(:).OmegaIndex ...
-    PI.H.IndividualParams(:).OmegaIndex])),1)];
-sigma_prior= [ repelem(1,length(PI.H.PopulationParams), 1);...
-    repelem(1, length([PI.H.CellParams(:).Index]),1);
-     repelem(1, length([PI.H.IndividualParams(:).Index]),1);...
-    alpha];
-lb=([3     9e-3    4   0.10    0.01	1.9    3e-3    4.9    0.07  1   1e-6    1e-6])';
-ub=([250   1.58    116.3 0.86    0.05	116    4e-2    39     0.8   1e5 1e6     1e6])';
+SigmaNames = getVarNames(PI, stateVar);
+[beta, sigma_prior] = getVarValues([0.1 0.1 .001], [0.1, 0.1 0.001], [1 1 1], PI);
+% Define lower and upper bounds for population params
+lb=([3     9e-3    4        0.10    0.01	1.9      1   1e-6    1e-6])';
+ub=([250   1.58    116.3    0.86    0.05	116      1e5 1e6     1e6])';
+% Generate parameter estimation structure
 PI.par = getParamStruct2(sim,PI.H,size(PI.data,1),beta,...
     SigmaNames,'Sigma', sigma_prior, 'ref', 'ones','LB', lb, 'UB', ub);
 try
@@ -103,13 +76,7 @@ end
 % Log-ikelihood function
 likelihood_fun=@(p)likelihood(exp(p),sim,PI,'censoring',false);
 prior_fun=@(p)(createPriorDistribution3(exp(p),PI,PI.H,'type',{'uniform/normal/inverse gamma/inverse gamma'}));
-
-% Residuals 
-residuals_fn = @(x) getResiduals(exp(x),@(x)sim(x,PI.tspan(end),PI.u,PI.tspan),PI,...
-    @(x)getPhi2(x,PI.H,length(PI.u),'initialValue',PI.x_0),...
-    exp(finalValues(end-length(observables)+1:end)),exp(finalValues([PI.H.CellParams.OmegaIndex])),...
-    exp(finalValues([PI.H.IndividualParams.OmegaIndex])),PI.normIndx);
-
+% Generate parameter names for plots
 paramNames = getParamNames(PI,sim, observables);
 %% Objective function
 
