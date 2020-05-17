b4ba43ff754fe7acfb4b6aba4ba6848b2d3af2da
Miguel <jm.tenorio.pedraza@gmail.com>
2020-05-17 15:37:27 +0200

    m

--- output/CIM/HuSim/PI_HuSim_ICB.m
+++ output/CIM/HuSim/PI_HuSim_ICB.m
@@ -12,8 +12,8 @@ out = sbioloadproject('/Users/migueltenorio/Documents/GitHub/CIT-SimBiology-Tool
 % Extract model
 model=out.m1;
 cs=model.getconfigset;
-set(cs.SolverOptions, 'AbsoluteTolerance', 1.0e-15);
-set(cs.SolverOptions, 'RelativeTolerance', 1.0e-12);
+set(cs.SolverOptions, 'AbsoluteTolerance', 1.0e-11);
+set(cs.SolverOptions, 'RelativeTolerance', 1.0e-9);
 set(cs, 'MaximumWallClock', 0.25)
 MODEL = 'CIM 4';
 variants = get(model, 'variants');
@@ -88,6 +88,9 @@ PI.u(1,1:2) = {control control};
 PI.u(2,1:2) = {antiPDL1_mono control};
 PI.u(3, 1:2) = {control antiCTLA4_mono};
 PI.u(4, 1:2) = {antiPDL1_combi antiCTLA4_combi};
+clearvars n_doses_antiPDL1_mono n_doses_antiCTLA4_mono dosing_times_antiPDL1_mono...
+    dosing_times_antiCTLA4_mono dosing_times_antiPDL1_combi maxDosingFreq doseScalingFactor ...
+    control antiPDL1_mono antiCTLA4_mono antiPDL1_combi antiPDL1_combi
 %% Hierarchical model simulation
 PI.H = getHierarchicalStruct(parameters(1:end-1),PI,'n_sigma', length(observables),...
     'rand_indx', [14:16],'cell_indx',[2 4 12 13], 'n_indiv', length(PI.u),'CellField', 'Name');
@@ -132,26 +135,223 @@ TV_range = [pi*4/3*.5^3 pi*4/3*2.5^3];
 TV = log((rand(size(theta,1),1)*(TV_range(2)-TV_range(1))+TV_range(1))/0.00153);
 Theta1(:,end) = TV;
 
-scalingExp1 = [0.8 0.8 0.8 0.8 0.8 0.8 0 0 0 0 0 0 0 0 0 -3/4 0 0 0 0 0 0];
+scalingExp1 = [0.8 0.8 0.8 0.8 0.8 0.8 0 0 0 0 0 0 0 0 0 -1/4 0 0 0 0 0 0];
+scalingExp2 = [0.8 0.8 0.8 0.8 0.8 0.8 0 0 0 0 0 0 0 0 0 -1/4 0 0 0 0 0 0];
 
 scalingFactor1 = (77/.022).^(scalingExp1);
 
 Theta1 = Theta1+log(scalingFactor1);
-table(parameters, exp(mean(Theta1))')
+Theta1 = exp(Theta1);
+table(parameters, exp(mean(log(Theta1))'))
+sigma_indx = randsample(size(PI_CIM.PI.postSamples,1),N_pop*N_indiv,true);
+sigma = nan(N_pop*N_indiv,2);
+sigma(:,2) = PI_CIM.PI.postSamples(sigma_indx,end-6);
+sigma_indx = randsample(size(PI_ICB.PI.postSamples,1),N_pop*N_indiv,true);
+sigma(:,1) = PI_ICB.PI.postSamples(sigma_indx,end);
+sigma = exp(sigma);
 %% Plot bivariate marginals
-plotBivariateMarginals_2(exp(Theta1(:,1:8)),...
-       'names',parameters([1:8]),'interpreter', 'tex')
+plotBivariateMarginals_2((Theta1(:,:)),...
+       'names',parameters,'interpreter', 'tex')
 plotCorrMat(Theta1, parameters)
 
 %% Posterior predictions
 simFun=@(x)getOutput2(PI,@(p)sim(p,PI.tspan(end),PI.u,PI.tspan),x,...
     {},PI.normIndx,'Output', 'data');
-dataOutput = simFun(exp(Theta1(1,:)));
+dataOutput = simFun((Theta1(1,:)));
 [PI.data(1:end).simValue] = dataOutput{:,:};
 figure
 hold on
 arrayfun(@(x)plot(PI.tspan, x.simValue(:,1), 'Color', x.colors), PI.data, 'UniformOutput', false)
 
 tic
-PI1=getPosteriorPredictions2(exp(Theta1(:,:)),PI,simFun,PI.observablesPlot);
+PI1=getPosteriorPredictions2((Theta1(:,:)),PI,simFun,PI.observablesPlot);
 toc
+
+%% Plot posterior predictions (Killing rate scaling exponent: -1/4]
+colors = linspecer(4);
+colors_i = zeros(size(Theta1,1),3);
+groups = {'Control' 'antiPDL1' 'antiCTLA4' 'antiPDL1_antiCTLA4'};
+response2 = table({'PD'; 'SD'; 'PR'; 'CR'},zeros(4,1),zeros(4,1),zeros(4,1),zeros(4,1),...
+    'VariableNames', {'Response' 'Control' 'antiPDL1' 'antiCTLA4' 'antiPDL1_antiCTLA4'});
+lineStyle = {'-' '-.' '--' ':'};
+response2_pred = response2;
+% Calculate SLD and response %
+for i=1:4
+    PI1.output(i).SLD = ((PI1.output(i).TV./(Theta1(:,end)*0.00153)).^(1/3))*100-100;
+    PI1.output(i).TV_sigma = exp(log(PI1.output(i).TV)+randn(N_indiv*N_pop,...
+        size(PI1.output(i).TV,2)).*sigma(:,1));
+    PI1.output(i).CD8_sigma = exp(log(PI1.output(i).CD8)+randn(N_indiv*N_pop,...
+        size(PI1.output(i).CD8,2)).*sigma(:,2));
+    PI1.output(i).SLD_sigma = ((PI1.output(i).TV_sigma./(Theta1(:,end)*0.00153)).^(1/3))*100-100;
+    
+    pd = and(any(PI1.output(i).SLD>20,2),...
+        any(PI1.output(i).TV/(4/3*pi).^(1/3)...
+        -(PI1.output(i).TV(:,1)/(4/3*pi)).^(1/3)>0.5,2));
+    
+    pr = and((and(any(PI1.output(i).SLD<=-30,2), ~any(PI1.output(i).SLD<-99,2))),~pd);
+    cr = and((any(PI1.output(i).SLD<-99,2)),~pd);
+    sd = and(and(~pd,~pr), ~cr);
+    response2{1:4,i+1} = [mean(pd); mean(sd); mean(pr); mean(cr)];
+
+    pd_sigma = (any(PI1.output(i).SLD_sigma>20,2));
+    pr_sigma = and((and(any(PI1.output(i).SLD_sigma<=-30,2), ~any(PI1.output(i).SLD_sigma<-99,2))),~pd);
+    cr_sigma = and((any(PI1.output(i).SLD_sigma<-99,2)),~pd);
+    sd_sigma = and(and(~pd_sigma,~pr_sigma), ~cr_sigma);
+    response2_pred{1:4,i+1} = [mean(pd_sigma); mean(sd_sigma); mean(pr_sigma); mean(cr_sigma)];
+    
+    colors_i(pd,:) = repmat(colors(1,:),sum(pd),1);
+    colors_i(sd,:) = repmat(colors(2,:),sum(sd),1);
+    colors_i(pr,:) = repmat(colors(3,:),sum(pr),1);
+    colors_i(cr,:) = repmat(colors(4,:),sum(cr),1);
+    PI1.output(i).Response = [pd sd pr cr];
+    PI1.output(i).Response_sigma = [pd_sigma sd_sigma pr_sigma cr_sigma];
+
+    PI1.output(i).colors = colors_i;
+end
+% Plot SLD
+indx = zeros(4,1);
+figure
+for i=1:4
+subplot(2,2,i)
+h=plot(PI1.tspan/7, PI1.output(i).SLD);
+
+for j=1:length(h)
+    h(j).Color = PI1.output(i).colors(j,:);
+end
+hold on
+plot(PI1.tspan/7, repelem(20,1,length(PI1.tspan)), '--k')
+plot(PI1.tspan/7, repelem(-30,1,length(PI1.tspan)), '--k')
+for k=1:4
+indx(k) = find(PI1.output(i).Response(:,k),1);
+end
+legend(h(indx),{'PD'; 'SD'; 'PR'; 'CR'})
+set(gca, 'YSCale', 'linear','YLim', [-100 100])
+xlabel('Time [weeks]')
+ylabel('Change in tumor diameter wrt baseline')
+title (strjoin({'Tumor response in virtual patients (' groups{i} ')'},''))
+end
+% Plot mean SLD
+figure('Renderer', 'painters', 'Position', [10 10 800 700])
+for i =1:4
+   subplot(2,2,i)
+   hold on
+   for j=1:4
+       mean_ij=mean(PI1.output(i).SLD(PI1.output(i).Response(:,j),:),'omitnan');
+       ci_ub = quantile(PI1.output(i).SLD(PI1.output(i).Response(:,j),:),.975);
+       ci_lb = quantile(PI1.output(i).SLD(PI1.output(i).Response(:,j),:),.025);
+       
+       pi_ub = quantile(PI1.output(i).SLD_sigma(PI1.output(i).Response(:,j),:),.975);
+       pi_lb = quantile(PI1.output(i).SLD_sigma(PI1.output(i).Response(:,j),:),.025);
+       
+    h=plot(PI1.tspan/7, mean_ij);
+    ci_plot = patch('XData', [PI1.tspan/7 PI1.tspan(end:-1:1)/7], 'YData', [ci_ub ci_lb(end:-1:1)]);
+    pi_plot = patch('XData', [PI1.tspan/7 PI1.tspan(end:-1:1)/7], 'YData', [pi_ub pi_lb(end:-1:1)]);
+    h.Color = colors(j,:);
+    h.LineWidth = 2;
+    ci_plot.FaceColor = colors(j,:);
+    ci_plot.LineStyle = lineStyle{j};
+    ci_plot.EdgeColor = colors(j,:);
+    ci_plot.FaceAlpha = 0.2;
+    
+    pi_plot.LineStyle = 'none';
+    pi_plot.FaceColor = colors(j,:);
+    pi_plot.FaceAlpha = 0.01;
+   end
+   ax = gca;
+   legend(ax.Children(end:-3:1),{'PD'; 'SD'; 'PR'; 'CR'})
+
+ set(gca, 'YSCale', 'linear','YLim', [-100 100])
+ xlabel('Time [weeks]')
+ylabel('Change in tumor diameter wrt baseline')
+title (strjoin({'Mean tumor response in virtual patients (' groups{i} ')'},''),'interpreter', 'none')
+
+end
+
+% Mean CD8
+figure('Renderer', 'painters', 'Position', [10 10 800 700])
+for i=1:4
+subplot(2,2,i)
+hold on
+ for j=1:4
+        mean_ij=mean(PI1.output(i).CD8(PI1.output(i).Response(:,j),:),'omitnan');
+       ci_ub = quantile(PI1.output(i).CD8(PI1.output(i).Response(:,j),:),.975);
+       ci_lb = quantile(PI1.output(i).CD8(PI1.output(i).Response(:,j),:),.025);
+       
+       pi_ub = quantile(PI1.output(i).CD8_sigma(PI1.output(i).Response(:,j),:),.975);
+       pi_lb = quantile(PI1.output(i).CD8_sigma(PI1.output(i).Response(:,j),:),.025);
+       
+    h=plot(PI1.tspan/7, mean_ij);
+    ci_plot = patch('XData', [PI1.tspan/7 PI1.tspan(end:-1:1)/7], 'YData', [ci_ub ci_lb(end:-1:1)]);
+    pi_plot = patch('XData', [PI1.tspan/7 PI1.tspan(end:-1:1)/7], 'YData', [pi_ub pi_lb(end:-1:1)]);
+    h.Color = colors(j,:);
+    h.LineWidth = 2;
+    ci_plot.FaceColor = colors(j,:);
+    ci_plot.LineStyle = lineStyle{j};
+    ci_plot.EdgeColor = colors(j,:);
+    ci_plot.FaceAlpha = 0.2;
+    
+    pi_plot.LineStyle = 'none';
+    pi_plot.FaceColor = colors(j,:);
+    pi_plot.FaceAlpha = 0.01;
+end
+     ax = gca;
+   legend(ax.Children(end:-3:1),{'PD'; 'SD'; 'PR'; 'CR'},'FontSize', 10)
+xlabel('Time [weeks]')
+ylabel('Percentage [%]')
+title (strjoin({'CD8+ T-cell response in virtual patients (' groups{i} ')'},''))
+ylim([0 5])
+end
+
+% Mean antiPDL1
+figure
+for i=1:4
+subplot(2,2,i)
+hold on
+ for j=1:4
+       mean_ij=mean(PI1.output(i).Tumor_antiPDL1(PI1.output(i).Response(:,j),:),'omitnan');
+       std_ij = std(PI1.output(i).Tumor_antiPDL1(PI1.output(i).Response(:,j),:),'omitnan');
+    h=errorbar(PI1.tspan/7, mean_ij,std_ij);
+    h.Color = colors(j,:);
+ end
+   legend({'PD'; 'SD'; 'PR'; 'CR'})
+
+xlabel('Time [weeks]')
+ylabel('Concentration [mg/L]')
+% ylim([0 0.5])
+title (strjoin({'anti-PD-L1 concentrations in tumor tissue of virtual patients (' groups{i} ')'},''))
+
+end
+
+% Mean antiCTLA4
+figure
+for i=1:4
+subplot(2,2,i)
+hold on
+ for j=1:4
+       mean_ij=mean(PI1.output(i).Tumor_antiCTLA4(PI1.output(i).Response(:,j),:),'omitnan');
+       std_ij = std(PI1.output(i).Tumor_antiCTLA4(PI1.output(i).Response(:,j),:),'omitnan');
+    h=errorbar(PI1.tspan/7, mean_ij,std_ij);
+    h.Color = colors(j,:);
+ end
+   legend({'PD'; 'SD'; 'PR'; 'CR'})
+
+xlabel('Time [weeks]')
+ylabel('Concentration [mg/L]')
+ylim([0 0.2])
+title (strjoin({'anti-PD-L1 concentrations in tumor tissue of virtual patients (' groups{i} ')'},''))
+
+end
+
+%% PFS in theta1
+% Calculate SLD and response %
+[PI1, response1] = getPFS(PI1, groups, 181);
+[PI1,T, censor] = getSurvivalTime(PI1, 181, groups, Theta1);
+plotSurvival(T,censor,Theta1,groups)
+% subplot(1,2,1)
+plotSurvivalFunction(PI1,181,groups)
+title('Progresson-free survival at 6 months (kill_{CD8} \propto M^{-0.75})')
+
+%% Analysing parameter-output relations
+corrInOut = plotInputToOutput(Theta1, {'SLD'}, PI1, groups, parameters);
+corrInOut2 = plotInputToOutput(PI1.Theta, {'SLD'}, PI1, groups, parameters,'plotOutput', true);
+
---
+++ output/CIM/PI/CIM21/PI_CIM21_ICB.m
@@ -0,0 +1,112 @@
+%% General setup for Kosinsky et al with k_pro as the parameter to vary for each individual
+%% Search paths
+warning on
+clear all
+addpath(genpath('/Users/migueltenorio/Documents/GitHub/CIT-SimBiology-Toolbox'))
+cd('/Users/migueltenorio/Documents/GitHub/CIT-SimBiology-Toolbox/output/CIM/PI/CIM21')
+
+%% Load project 
+out = sbioloadproject('/Users/migueltenorio/Documents/GitHub/CIT-SimBiology-Toolbox/sbio projects/CIM_4.sbproj');
+% Extract model
+model=out.m1;
+variants = getvariant(model);
+initialStruct = struct('name', {'MOC1';'MOC2'}, 'initialValue', {5; 0.1;},...
+    'variant', {variants(5); variants(6)});
+
+cs=model.getconfigset;
+set(cs.SolverOptions, 'AbsoluteTolerance', 1.0e-11);
+set(cs.SolverOptions, 'RelativeTolerance', 1.0e-9);
+set(cs, 'MaximumWallClock', 0.25)
+sbioaccelerate(model, cs)
+%% Parameter setup
+parameters = {'kpro_Tumor';'kpro_Tumor_Linear';'kill_CD8'; 'S_L';...
+    'S_R'; 'K_CTLA4';'K_PDL1'; 'kill_Treg'};
+parameters = [parameters; 'kin_MDSC';'kin_TIC';  'T_0'];
+
+% Define outputs% Define outputs
+groups_subset = {'MOC1_antiCTLA4', 'MOC1_antiPDL1', 'MOC1_antiCTLA4_antiPDL1' 'MOC2_antiCTLA4',...
+    'MOC2_antiPDL1' 'MOC2_antiCTLA4_antiPDL1'};
+observables={'TV'};
+stateVar={'Tumor'  };
+doses = {'Blood.Dose_antiPDL1' 'Blood.Dose_antiCTLA4'};
+%% Obtain data, simulation function and dose table
+
+PI=getPIData3('/Users/migueltenorio/Documents/GitHub/CIT-SimBiology-Toolbox/data/PI_Clavijo.mat',...
+    stateVar,groups_subset,'output', 'mean','responseGrouping', true, 'kineticGrouping', true);
+PI.variableUnits={'Volume [mL]' 'Percentage [%]' 'Percentage [%]'  'Percentage [%]' ...
+     'Percentage [%]'   'Relative units []' ...
+    'Relative units []' 'Relative units []'};
+PI.normIndx = [];
+PI.model = 'CIM21 ICB';
+PI.observablesPlot={'TV'};
+plotData(PI,PI.observablesPlot,'responseGrouping',true, 'kineticGrouping',true)
+% Get initial values
+[PI.x_0, PI.variants] = getInitialValues([PI.data(:).Group],...
+    initialStruct, 'parameters', {'kin_MDSC' 'kin_TIC'});
+
+% Get simulation function
+[sim,PI.u]=initializePI(model,parameters,observables,PI,doses, 'MOC1_optimized','doseUnits', 'mole');
+
+%% Optimization setup
+% Hierarchical structure
+PI.H = getHierarchicalStruct(parameters(1:end-3),PI,'n_sigma', length(observables),...
+    'rand_indx', [1:3] , 'cell_indx',[], 'n_indiv', length(PI.u));
+SigmaNames = getVarNames(PI, stateVar);
+[beta, sigma_prior] = getVarValues([1 1 .001], [1, 1 0.001], [1 1 1], PI);
+
+lb=([.07   1.6  150    1e-7     1e-7   1e0   1e3    1e-4])';
+ub=([.3    8    650    1        1      1e4   1e7     1e3])';
+PI.par = getParamStruct2(sim,PI.H,size(PI.data,1),beta,...
+    SigmaNames,'Sigma', sigma_prior, 'ref', 'ones','LB', lb, 'UB', ub);
+
+PI = assignPrior(PI);
+% Log-ikelihood function
+likelihood_fun=@(p)likelihood(exp(p),sim,PI,'censoring',false);
+prior_fun_MCMC=@(p)getPriorPDFMCMC2(exp(p),PI);
+
+paramNames = getParamNames(PI,sim, observables);
+%% Objective function
+try
+    finalValues =log([PI.par(:).finalValue]);
+catch
+    finalValues =log([PI.par(:).startValue]);
+
+end
+% Obj function
+obj_fun=@(x)(likelihood_fun(x)*(-1)+prior_fun_MCMC(x)*(-1));
+tic
+obj_fun((finalValues))
+toc
+clearvars ans beta doses groups_subset initialStruct lb observables out sigma_prior SigmaNames stateVar ub variants
+%% Parameter selection of inter-cell line varying params
+
+w = arrayfun(@(x) (finalValues(x.Index)), PI.H.CellParams,'UniformOutput', false);
+w = (std(cell2mat(w'),[], 2));
+[w,cell_indx] =sort(w,'descend');
+cell_params = [ {PI.H.CellParams(:).name}'];
+
+z = arrayfun(@(x) (finalValues(x.Index)), PI.H.IndividualParams,'UniformOutput', false);
+z = (std(cell2mat(z'),[], 2));
+[z,ind_indx] =sort(z,'descend');
+ind_params = [{PI.H.IndividualParams(:).name}'];
+
+table([cell_params(cell_indx); ind_params(ind_indx)], [w; z])
+%% Save results
+save('PI_CIM4_ICB_21_1.mat', 'PI')
+load(strjoin({cd 'PI_CIM4_ICB_21_1.mat'},'/'),'PI')
+
+save(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_x_3.mat'},''), 'x3')
+save(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_p_x_3.mat'},''), 'p_x3')
+save(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_J3.mat'},''), 'J3')
+save(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_n_id3.mat'},''), 'n_id3')
+save(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_pCR3.mat'},''), 'pCR3')
+
+load(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_x_3.mat'},''))
+load(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_p_x_3.mat'},''))
+load(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_J3.mat'},''))
+load(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_n_id3.mat'},''))
+load(strjoin({cd '/PI_CIM21_ICB_1_DREAM_MCMC_pCR3.mat'},''))
+
+load(strjoin({cd 'PI_CIM21_ICB_1_DREAM_MCMC_x_1.mat'},'/'))
+load(strjoin({cd 'DREAM_MCMC_logP.mat'},'/'))
+
